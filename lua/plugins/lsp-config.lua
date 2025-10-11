3163f6454658d31905292384417d95356c2729e6
shinokun <hypatia.tile02021@gmail.com>
2025-10-12 00:01:26 +0900

    Fix diagnostics reference in lsp-config.lua and add Lua diagnostics settings for vim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -6,7 +6,7 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "codelldb" })
-      if diagnostics == "bacon-ls" then
+      if vim.diagnostics == "bacon-ls" then
         vim.list_extend(opts.ensure_installed, { "bacon" })
       end
     end,
