d9d7e8a85544ffa3d648944902f83c072177ea1b
shinokun <hypatia.tile02021@gmail.com>
2025-10-17 00:03:37 +0900

    Refactor Neovim configuration for clarity and add user command to open init.lua

--- init.lua
+++ init.lua
@@ -3,3 +3,10 @@ vim.notify("Welcome to Neovim!", vim.log.levels.INFO, { title = "Neovim" })
 require "vim-options"
 require "config.lazy"
 require "plugins.float_note"
+
+vim.api.nvim_create_user_command("InitLua", function()
+  vim.notify("Open init.lua", vim.log.levels.INFO)
+  vim.cmd.edit(vim.fn.stdpath "config" .. "/init.lua")
+end, {
+  desc = "Open init.lua",
+})
