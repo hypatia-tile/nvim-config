823b6d226fff3aa5c7bdfc7620f4ceb8d57172ab
shinokun <hypatia.tile02021@gmail.com>
2025-10-26 00:03:13 +0900

    reduce: set minimal configuration for rustaceanvim since this plugin is a filetype plugin that works out of the box.

--- lua/plugins/rustaceanvim.lua
+++ lua/plugins/rustaceanvim.lua
@@ -4,7 +4,4 @@ return {
   "mrcjkb/rustaceanvim",
   version = "^6",
   ft = { "rust" }, -- load only for Rust files
-  config = function()
-    vim.notify("rustaceanvim loaded", vim.log.levels.INFO)
-  end,
 }
