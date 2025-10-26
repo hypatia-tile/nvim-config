cb2a31472a1d402e8d7beebb6c667feaa5fdeba9
shinokun <hypatia.tile02021@gmail.com>
2025-10-26 19:39:05 +0900

    refactor Neovim settings
    define a small function in another place

--- init.lua
+++ init.lua
@@ -3,10 +3,5 @@ vim.notify("Welcome to Neovim!", vim.log.levels.INFO, { title = "Neovim" })
 require "vim-options"
 require "config.lazy"
 require "plugins.float_note"
-
-vim.api.nvim_create_user_command("InitLua", function()
-  vim.cmd.edit(vim.fn.stdpath "config" .. "/init.lua")
-  vim.notify("Open init.lua", vim.log.levels.INFO)
-end, {
-  desc = "Open init.lua",
-})
+require "lsp"
+require "shino.commands"
