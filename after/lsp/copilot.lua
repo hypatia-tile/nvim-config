891796cdf43584836e091a6bce248a1127ebb3c5
shinokun <hypatia.tile02021@gmail.com>
2025-10-29 01:53:07 +0900

    feat: add some comment and set tab as accepting key bind for copilot

--- after/lsp/copilot.lua
+++ after/lsp/copilot.lua
@@ -1,4 +1,6 @@
 return {
+  -- See :h lsp-root_dir
+  -- lsp-root_dir() decides the workspace root
   root_dir = function(bufnr, callback)
     local fname = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr))
     local disable_patterns = { "env", "conf", "local", "private" }
@@ -15,11 +17,17 @@ return {
     end
   end,
   on_init = function()
+    -- If there exists a complition item accept it, otherwise fallback to tab
+    vim.keymap.set("i", '<tab>', function()
+      if not vim.lsp.inline_completion.get() then
+        return '<tab>'
+      end
+    end, { expr = true })
+    -- TODO: investigate about higlight groups in a namespace
     local hlc = vim.api.nvim_get_hl(0, { name = "Comment" })
     vim.api.nvim_set_hl(0, "ComplHint", vim.tbl_extend("force", hlc, { underline = true }))
     local hlm = vim.api.nvim_get_hl(0, { name = "MoreMsg" })
     vim.api.nvim_set_hl(0, "ComplHintMore", vim.tbl_extend("force", hlm, { underline = true }))
-
     vim.api.nvim_create_autocmd("LspAttach", {
       callback = function(args)
         local bufnr = args.buf
