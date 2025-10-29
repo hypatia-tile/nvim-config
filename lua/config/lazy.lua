75a7b1c24b38fa9964d1faac01dfefc89ab9d196
shinokun <hypatia.tile02021@gmail.com>
2025-10-29 10:40:52 +0900

    add debug adapter

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -5,7 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
+      { out,                            "WarningMsg" },
       { "\nPress any key to exit..." },
     }, true, {})
     vim.fn.getchar()
@@ -17,7 +17,7 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup {
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins" },
+    { "LazyVim/LazyVim",              import = "lazyvim.plugins" },
     -- import/override with your plugins
     { import = "plugins.catppuccin" },
     { import = "plugins.completion" },
@@ -27,6 +27,7 @@ require("lazy").setup {
     { import = "plugins.tex" },
     { import = "plugins.treesitter" },
     { import = "plugins.rustaceanvim" },
+    { import = "plugins.bap" },
   },
   defaults = {
     -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
---
+++ lua/plugins/bap.lua
@@ -0,0 +1,8 @@
+return {
+  "mfussenegger/nvim-dap",
+  lazy = false,
+  config = function()
+    -- TODO do something
+    vim.notify("nvim-dap configured", vim.log.levels.INFO)
+  end
+}
