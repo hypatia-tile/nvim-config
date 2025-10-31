843510c2162d0a61553efc1761760df4130df57a
shinokun <hypatia.tile02021@gmail.com>
2025-10-31 14:22:09 +0900

    Nvim LSP: Added TypeScript language server configuration.

---
+++ lsp/ts_ls.lua
@@ -0,0 +1,91 @@
+return {
+  cmd = { "ts_ls" },
+  filetypes = {
+    "javascript",
+    "javascriptreact",
+    "javascript.jsx",
+    "typescript",
+    "typescriptreact",
+    "typescript.tsx",
+  },
+  settings = {
+    complete_function_calls = true,
+    vtsls = {
+      enableMoveToFileCodeAction = true,
+      autoUseWorkspaceTsdk = true,
+      experimental = {
+        maxInlayHintLength = 30,
+        completion = {
+          enableServerSideFuzzyMatch = true,
+        },
+      },
+    },
+    typescript = {
+      updateImportsOnFileMove = { enabled = "always" },
+      suggest = {
+        completeFunctionCalls = true,
+      },
+      inlayHints = {
+        enumMemberValues = { enabled = true },
+        functionLikeReturnTypes = { enabled = true },
+        parameterNames = { enabled = "literals" },
+        parameterTypes = { enabled = true },
+        propertyDeclarationTypes = { enabled = true },
+        variableTypes = { enabled = false },
+      },
+    },
+  },
+  keys = {
+    {
+      "gD",
+      function()
+        local win = vim.api.nvim_get_current_win()
+        local params = vim.lsp.util.make_position_params(win, "utf-16")
+        LazyVim.lsp.execute({
+          command = "typescript.goToSourceDefinition",
+          arguments = { params.textDocument.uri, params.position },
+          open = true,
+        })
+      end,
+      desc = "Goto Source Definition",
+    },
+    {
+      "gR",
+      function()
+        LazyVim.lsp.execute({
+          command = "typescript.findAllFileReferences",
+          arguments = { vim.uri_from_bufnr(0) },
+          open = true,
+        })
+      end,
+      desc = "File References",
+    },
+    {
+      "<leader>co",
+      LazyVim.lsp.action["source.organizeImports"],
+      desc = "Organize Imports",
+    },
+    {
+      "<leader>cM",
+      LazyVim.lsp.action["source.addMissingImports.ts"],
+      desc = "Add missing imports",
+    },
+    {
+      "<leader>cu",
+      LazyVim.lsp.action["source.removeUnused.ts"],
+      desc = "Remove unused imports",
+    },
+    {
+      "<leader>cD",
+      LazyVim.lsp.action["source.fixAll.ts"],
+      desc = "Fix all diagnostics",
+    },
+    {
+      "<leader>cV",
+      function()
+        LazyVim.lsp.execute({ command = "typescript.selectTypeScriptVersion" })
+      end,
+      desc = "Select TS workspace version",
+    },
+  },
+}
