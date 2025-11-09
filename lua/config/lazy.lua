1229253c066307348d4aa072c3c0ba43d4f7b58b
shinokun <hypatia.tile02021@gmail.com>
2025-11-09 16:34:14 +0900

    Refactored Neovim configuration * refactor lazy.lua to improve plugin management and loading times

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,31 +14,35 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup {
-  spec = {
-    -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim",              import = "lazyvim.plugins" },
-    -- import/override with your plugins
-    { import = "plugins.catppuccin" },
-    { import = "plugins.completion" },
-    { import = "plugins.lsp-config" },
-    { import = "plugins.lualine" },
-    { import = "plugins.telescope" },
-    { import = "plugins.tex" },
-    { import = "plugins.treesitter" },
-    { import = "plugins.rustaceanvim" },
-    { import = "plugins.bap" },
-    { import = "plugins.autopairs" },
+local plugins = {
+  -- add LazyVim and import its plugins
+  {
+    "LazyVim/LazyVim",
+    import = "lazyvim.plugins"
   },
-  defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
-    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-    -- have outdated releases, which may break your Neovim install.
-    version = false, -- always use the latest git commit
-    -- version = "*", -- try installing the latest stable version for plugins that support semver
-  },
+  -- import/override with your plugins
+  { import = "plugins.catppuccin" },
+  { import = "plugins.completion" },
+  { import = "plugins.lsp-config" },
+  { import = "plugins.lualine" },
+  { import = "plugins.telescope" },
+  { import = "plugins.tex" },
+  { import = "plugins.treesitter" },
+  { import = "plugins.rustaceanvim" },
+  { import = "plugins.bap" },
+  { import = "plugins.autopairs" },
+}
+
+local opts = {
+  root = vim.fn.stdpath("data") .. "/lazy",
+  lockfile = vim.fn.stdpath("config") .. "/lazy-lock.json",
+  concurrency = 10,
+  checker = { enabled = true },
+  log = { level = "info" },
+}
+
+require("lazy").setup({
+  spec = plugins,
   install = { colorscheme = { "tokyonight", "habamax" } },
   checker = {
     enabled = true,
@@ -55,4 +59,4 @@ require("lazy").setup {
       },
     },
   },
-}
+}, opts)
