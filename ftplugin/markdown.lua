6ebb4a74a3d07606c7bb056bf6282f9c01eeb673
shinokun <hypatia.tile02021@gmail.com>
2025-12-06 10:56:50 +0900

    Configure markdown ftplugin #3

--- ftplugin/markdown.lua
+++ ftplugin/markdown.lua
@@ -3,3 +3,7 @@ vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
 vim.opt_local.spell = true
 vim.opt_local.conceallevel = 2
+vim.g.mkdp_auto_start = 0 -- Set to 1 to auto start markdown preview window after entering markdown buffer
+vim.g.mkdp_auto_close = 1 -- Set to 1 to auto close current preview window when changing from Markdown buffer
+vim.g.mkdp_refresh_slow = 0
+vim.keymap.set("n", "<leader>mp", ":MarkdownPreview<CR>", { buffer = true, desc = "Markdown Preview" })
