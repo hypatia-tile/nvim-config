b9594f660548e915cc963368b35efe5e40d0e82f
shinokun <hypatia.tile02021@gmail.com>
2026-04-17 00:47:58 +0900

    bugfix: Fix skkeleton

--- lua/plugins/denops.lua
+++ lua/plugins/denops.lua
@@ -1,4 +1,9 @@
 return {
   "vim-denops/denops.vim",
   lazy = false, -- Load on startup
+  priority = 500,
+  config = function()
+    -- Enable denops debug logging
+    vim.g["denops#debug"] = 1
+  end,
 }
--- lua/plugins/skkeleton.lua
+++ lua/plugins/skkeleton.lua
@@ -1,3 +1,19 @@
+-- skkeleton {{{
 return {
   "vim-skk/skkeleton",
+  dependencies = { "vim-denops/denops.vim" },
+  lazy = false, -- Load on startup so denops can discover it
+  keys = {
+    { "<C-j>", "<Plug>(skkeleton-toggle)", desc = "Toggle skkeleton", mode = "i" },
+  },
+  config = function()
+    vim.fn["skkeleton#config"] {
+      globalDictionaries = {
+        vim.fn.expand(vim.fn.stdpath "config" .. "/skk/SKK-JISYO.L"),
+      },
+      userDictionary = vim.fn.expand(vim.fn.stdpath "config" .. "/skk/user-dict"),
+      eggLikeNewline = true,
+    }
+  end,
 }
+-- }}}
